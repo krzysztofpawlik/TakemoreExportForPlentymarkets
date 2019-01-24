75c79d946b1990e13aa1a3b2ecbe4425a23f794d
Krzysztof Pawlik <krzysztof.pawlik@mediaport.pl>
2019-01-24 15:33:08 +0100

    Price test

--- src/Generator/TakemoreExportFormatGenerator.php
+++ src/Generator/TakemoreExportFormatGenerator.php
@@ -171,7 +171,7 @@ class TakemoreExportFormatGenerator extends CSVPluginGenerator
 			$salesPriceSearchRequest->currency = $priceList['currency'];
 		}
 		$salesPriceSearch  = $this->salesPriceSearchRepositoryContract->search($salesPriceSearchRequest);
-		$price = $salesPriceSearch['price'];
+		$price = $salesPriceSearch->price;
 
 		/* unnecessary $basePriceList = $this->elasticExportPriceHelper->getBasePriceDetails($variation, (float) $priceList['price'], $settings->get('lang'));
 		$deliveryCost = $this->elasticExportCoreHelper->getShippingCost($variation['data']['item']['id'], $settings); */
