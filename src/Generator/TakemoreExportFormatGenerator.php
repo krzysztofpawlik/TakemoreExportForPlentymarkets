af9e2ca9f8c53d3300fa12e3e5bbeccae1f08b8b
Krzysztof Pawlik <krzysztof.pawlik@mediaport.pl>
2019-07-24 11:54:16 +0200

    using getSpecificImageList

--- src/Generator/TakemoreExportFormatGenerator.php
+++ src/Generator/TakemoreExportFormatGenerator.php
@@ -161,8 +161,9 @@ class TakemoreExportFormatGenerator extends CSVPluginGenerator
 		$priceList = $this->elasticExportPriceHelper->getPriceList($variation, $settings, 2, '.');
 		$size = $this->elasticExportCoreHelper->getAttributeValueSetShortFrontendName($variation, $settings);
 		//$images = implode(',', $this->elasticExportCoreHelper->getImageListInOrder($variation, $settings, 10, ElasticExportCoreHelper::VARIATION_IMAGES));
-		$images = implode(',', $this->elasticExportCoreHelper->getImageList($variation, $settings));
+		//$images = implode(',', $this->elasticExportCoreHelper->getImageList($variation, $settings));
 		//$images = implode(',', $this->getVariationImageList($variation, $settings));
+		$images = implode(',', $this->elasticExportCoreHelper->getSpecificImageList($variation, $settings, 10,  ElasticExportCoreHelper::VARIATION_IMAGES, 'normal'));
 		$properties = $variation['data']['properties'];
 		$stockList = $this->variationStockRepositoryContract->listStockByWarehouse($variation['id']);
 		$stock = 0;
